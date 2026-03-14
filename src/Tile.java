f16bc81adcdce84b5c8d9e39db502983061a6452
Noe Game <noe.gme@reseau.eseo.fr>
2026-03-14 22:41:26 +0100

    fix(tile): Using proper name for neightbour designation

--- src/Tile.java
+++ src/Tile.java
@@ -1,8 +1,13 @@
 public class Tile {
-    Tile up;
+    Tile above;
     Tile right;
     Tile left;
-    Tile bottom;
+    Tile below;
+
+    boolean isTileLeft =    this.left != null;
+    boolean isTileRight =   this.right != null;
+    boolean isTileAbove =      this.above != null;
+    boolean isTileBelow =    this.below != null;
 
     Tile getLeft()
     {
@@ -14,20 +19,34 @@ public class Tile {
         return this.right;
     }
 
-    Tile getBottom()
+    Tile getBelow()
     {
-        return this.bottom;
+        return this.below;
     }
 
-    Tile getUp()
+    Tile getAbove()
     {
-        return this.up;
+        return this.above;
     }
 
     Tile(){
-        this.up = null;
-        this.bottom = null;
+        this.above = null;
+        this.below = null;
         this.right = null;
         this.left = null;
+    }
+
+
+
+    void show(){
+        System.out.println("I have a tile left: " + Boolean.toString(isTileLeft));
+
+        System.out.println("I have a tile right: "+ Boolean.toString(isTileRight));
+
+        System.out.println("I have a tile above: "+ Boolean.toString(isTileAbove));
+
+        System.out.println("I have a tile below: "+ Boolean.toString(isTileBelow));
+
+
     }
 }
